2a7269f3d0eabd4f201d70a4184e061e481b41df
rralph091 <60122569+rralph091@users.noreply.github.com>
2020-03-19 17:47:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,42 @@ $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET[
 ?>
 <html> 
     <head>
-      <title>ET #1</title>
-      <link rel="stylesheet" type="text/css" href="style.css">
+      <title>Endterm Activity #1</title>
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan|Fira+Sans|Mukta&display=swap" rel="stylesheet">
     </head>
     <body>
-      <?php include ('sidebar.php'); ?>
+     
 
+      <div class="navbar">
+        <a href="index.php?navigation=product">Products</a>
+        <a href="index.php?navigation=categories">Category</a>
+        <a href="index.php?navigation=create">Create</a>
+      </div>
 
-    
+      <div>         
+      <?php 
+        switch($navigation){
+          case 'product':
+            require_once 'product.php';
+            break;
+          case 'categories':
+            require_once 'categories.php';
+            break;
+          case 'create':
+            require_once 'form_create.php';
+            break;
+          case 'details':
+            require_once 'product-details.php';
+            break;
+		  case 'update':
+            require_once 'form_update.php';
+            break;
+        }
+    ?>
+      </div>
+
+     
     </body>
 </html>
+
